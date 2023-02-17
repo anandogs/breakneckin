456c729d5e1f9f756f0ea652671bdce86e0c7578
Anando Ghose <anandoghose@gmail.com>
2023-02-17 07:14:20 +0530

    till privacy

---
+++ src/components/CheckFields.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    export let data: any;
+    import { onMount } from "svelte";
+    onMount(() => console.log(data))
+</script>
--- src/components/PutNameForm.svelte
+++ src/components/PutNameForm.svelte
@@ -21,8 +21,10 @@
   let emailCheck: boolean = true;
   let phoneCheck: boolean = true;
   let optInCheck: boolean = true;
+  let invalidSubmission: boolean = false;
 
   const updateValue = () => {
+    invalidSubmission = false;
     if (name !== "") {
       nameCheck = true;
     }
@@ -62,6 +64,7 @@
 
   const handleSubmit = async (event: any) => {
     event.preventDefault();
+    
 
     if (name === "") {
       nameCheck = false;
@@ -82,6 +85,7 @@
       phoneCheck === false ||
       optIn === false
     ) {
+        invalidSubmission = true;
       return;
     }
 
@@ -224,7 +228,7 @@
       </p>
     </div>
     <div class="submission">
-      <button type="submit"> DONE </button>
+      <button type="submit" class:shake={invalidSubmission}> DONE </button>
     </div>
   </form>
 </section>
@@ -310,6 +314,30 @@
   button {
     background: #ff4800;
   }
+
+  .shake {
+    animation: shake 0.82s cubic-bezier(.36,.07,.19,.97) both;
+  transform: translate3d(0, 0, 0);
+  backface-visibility: hidden;
+  }
+
+  @keyframes shake {
+  10%, 90% {
+    transform: translate3d(-1px, 0, 0);
+  }
+  
+  20%, 80% {
+    transform: translate3d(2px, 0, 0);
+  }
+
+  30%, 50%, 70% {
+    transform: translate3d(-4px, 0, 0);
+  }
+
+  40%, 60% {
+    transform: translate3d(4px, 0, 0);
+  }
+}
 
   @media (min-width: 1024px) {
     p {
